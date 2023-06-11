7de020bcad666ca15491880bb468fdc08bb6653b
Zheng Wenjing <3504326499@qq.com>
2023-06-11 21:48:50 +0800

    login,register,logout,creatGroup代码中加入了输入的预处理

--- creatGroup.php
+++ creatGroup.php
@@ -5,7 +5,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $redis = new Redis();
 
   try {
-    $redis->pconnect('127.0.0.1');
+    $redis->connect('127.0.0.1');
     $redis->select(1);
 
     $group = json_decode(file_get_contents('php://input'), true);
@@ -13,16 +13,40 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $groupname = $group['groupname'];
     $description = $group['description']??'无';
 
-    // 设置组信息
-    $redis->hSet("$username:$groupname", 'name', "$groupname");
-    $redis->hSet("$username:$groupname", 'description', "$description");
-//    // 添加成员
-//    $redis->sAdd('mygroup:creator123:members', 'member1');
-//    $redis->sAdd('mygroup:creator123:members', 'member2');
-    // 关闭Redis连接
-    $redis->close();
+    // 验证用户名
+    if (!preg_match('/^[a-zA-Z0-9_]{3,20}$/', $username)) {
+      $code = 400;
+      $message = "用户名不合法";
+    }
+    // 验证群组名
+    elseif (!preg_match('/^[a-zA-Z0-9_]{3,20}$/', $groupname)) {
+      $code = 400;
+      $message = "群组名不合法";
+    }
+    // 验证描述
+    elseif (strlen($description) > 200) {
+      $code = 400;
+      $message = "描述过长";
+    }
+    else {
+      // 设置组信息
+      $redis->hSet("$username:$groupname", 'name', "$groupname");
+      $redis->hSet("$username:$groupname", 'description', "$description");
+      // 关闭Redis连接
+      $redis->close();
+      
+      $code = 200;
+      $message = "创建成功";
+    }
   } catch (RedisException $e) {
     $code = 503;
     $message = '服务出错，请稍后重试';
   }
+  
+  header('Content-Type: application/json');
+  $response = [
+    'code' => $code,
+    'message' => $message
+  ];
+  echo json_encode($response);
 }
--- register.php
+++ register.php
@@ -5,14 +5,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $redis = new Redis();
 
   try {
-    $redis->pconnect('127.0.0.1');
+    $redis->connect('127.0.0.1');
 
     $user = json_decode(file_get_contents('php://input'), true);
     $username = $user['username'];
     $password = $user['password'];
     $captcha = $user['captcha'];
 
-    if (isset($_SESSION['captchaID']) && !empty($_SESSION['captchaID']) && isset($_SESSION['captcha']) && !empty($_SESSION['captcha'])) {
+    // 验证用户名
+    if (!preg_match('/^[a-zA-Z0-9_]{3,20}$/', $username)) {
+      $code = 400;
+      $message = "用户名不合法";
+    }
+    // 验证密码
+    elseif (!preg_match('/^[a-zA-Z0-9]{3,20}$/', $password)) {
+      $code = 400;
+      $message = "密码不合法";
+    }
+    // 验证验证码
+    elseif (!preg_match('/^[a-zA-Z]{4}$/', $captcha)) {
+      $code = 400;
+      $message = "验证码不合法";
+    }
+    elseif (isset($_SESSION['captchaID']) && !empty($_SESSION['captchaID']) && isset($_SESSION['captcha']) && !empty($_SESSION['captcha'])) {
       if (strtolower($captcha) != strtolower($_SESSION['captcha'])) {
         $tmp = $_SESSION['captcha'];
         $code = 503;
