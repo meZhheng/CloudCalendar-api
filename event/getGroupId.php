e08ff7d4b27fa26f053c7a5a2599c2abc706c834
bloom667 <76032827+bloom667@users.noreply.github.com>
2023-06-17 20:18:11 +0800

    绑定组id和事件

--- event/getGroupId.php
+++ event/getGroupId.php
@@ -1,3 +1,40 @@
 <?php
 
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    try {
+        require_once "../checkAuth.php";
+        $authMiddleware = new AuthMiddleware();
+        $username = $authMiddleware->handle();
+        $redis = new Redis();
+        $redis->connect('127.0.0.1');
+        $redis->select(1);
+        $userGroups = $redis->sMembers("userGroup:$username");
+        $groupInfo = [];
+
+        foreach ($userGroups as $groupID) {
+            $groupName = $redis->hget("group:$groupID", "groupname");
+            $groupOption = [
+                'id' => $groupID,
+                'name' => $groupName,
+            ];
+            $groupInfo[] = $groupOption;
+        }
+
+        $code = 200;
+    } catch (RedisException $e) {
+        $code = 503;
+        $message = "服务出错，请稍后重试";
+    } catch (AuthFailedException $e) {
+        $code = 401;
+        $message = $e->getMessage();
+    }
+
+    header('Content-Type: application/json');
+    $response = [
+        'code' => $code,
+        'groupInfo' => $groupInfo ?? null,
+    ];
+    echo json_encode($response);
+}
+
 
