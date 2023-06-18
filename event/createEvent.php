0c9b99f06840726912fa8ef65fc7a0c45341f61a
meZhheng <93984838+meZhheng@users.noreply.github.com>
2023-06-18 16:03:18 +0800

    修复bug，最后一次正式更新

--- event/createEvent.php
+++ event/createEvent.php
@@ -7,22 +7,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     [$redis, $username] = $authMiddleware->handle();
     $event = json_decode(file_get_contents('php://input'), true);
     $eventCalendar = $event['eventCalendar'];
-    $groupID = $eventCalendar['selectedOptions'];
-    $redis->select(2);
-    $eventID = $redis->incr("event_id");
-    $redis->hmset("event:$eventID", $eventCalendar);
-    $redis->set("event:$eventID:creator", $username);
+    if($groupID = $eventCalendar['selectedOptions'] and $groupID != '') {
+      $redis->select(2);
+      $eventID = $redis->incr("event_id");
+      $redis->hmset("event:$eventID", $eventCalendar);
+      $redis->set("event:$eventID:creator", $username);
 
-    $redis->select(1);
-    $redis->sAdd("group:$groupID:events", $eventID);
-    $code = 200;
-    $message = "日程创建成功";
-    $schedule = [
-      '_id' => $eventID,
-      'title' => $eventCalendar['title'],
-      'start' => $eventCalendar['start'] ,
-      'endStr' => $eventCalendar['end'] ,
-    ];
+      $redis->select(1);
+      $redis->sAdd("group:$groupID:events", $eventID);
+      $code = 200;
+      $message = "日程创建成功";
+      $schedule = [
+        '_id' => $eventID,
+        'title' => $eventCalendar['title'],
+        'start' => $eventCalendar['start'] ,
+        'endStr' => $eventCalendar['end'] ,
+      ];
+    } else {
+      $code = 400;
+      $message = "请选择一个小组";
+    }
   } catch (RedisException $e) {
     $code = 503;
     $message = "服务出错，请稍后重试";
